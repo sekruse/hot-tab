7b38291a53a72923231d65e78c82a266e67419fd
Sebastian Kruse <sebastian@kruse.top>
2025-04-13 18:09:15 +0200

    make the list of pinned tabs look neater

--- popup.js
+++ popup.js
@@ -22,7 +22,21 @@ document.addEventListener('DOMContentLoaded', async () => {
   Object.keys(pins).sort().forEach((key) => {
     const pin = pins[key];
     const li = document.createElement('li');
-    li.innerText = `${key}: ${pin.title}`;
+    li.classList.add('flex', 'flex-row');
+    const keySpan = document.createElement('span');
+    keySpan.classList.add('key');
+    keySpan.innerText = key.replace('Key', '');
+    li.appendChild(keySpan);
+    if (pin.favIconUrl) {
+      const icon = document.createElement('img');
+      icon.classList.add('icon', 'margin-left');
+      icon.setAttribute('src', pin.favIconUrl)
+      li.appendChild(icon);
+    };
+    const title = document.createElement('span');
+    title.classList.add('margin-left');
+    title.innerText = pin.title;
+    li.appendChild(title);
     pinnedTabsList.appendChild(li);
   });
 });
